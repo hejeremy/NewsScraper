2ea979f596d72019899ef4c9bcfd7fbf04d0dc89
jeremyhe <jeremyhe1@gmail.com>
2017-09-29 23:45:41 -0400

    stuff

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
 
 // Initialize Express
 var app = express();
@@ -9,72 +12,74 @@ var app = express();
 app.use(express.static("public"));
 
 // Database configuration
-// Save the URL of our database as well as the name of our collection
-var databaseUrl = "zoo";
-var collections = ["animals"];
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
 
-// Use mongojs to hook the database to the db variable
+// Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
-
-// This makes sure that any errors are logged if mongodb runs into an issue
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-// Routes
-// 1. At the root path, send a simple hello world message to the browser
+// Main route (simple Hello World Message)
 app.get("/", function(req, res) {
   res.send("Hello world");
 });
 
-// 2. At the "/all" path, display every entry in the animals collection
+// Retrieve data from the db
 app.get("/all", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything
-  db.animals.find({}, function(error, found) {
-    // Log any errors if the server encounters one
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
     if (error) {
       console.log(error);
     }
-    // Otherwise, send the result of this query to the browser
+    // If there are no errors, send the data to the browser as json
     else {
       res.json(found);
     }
   });
 });
 
-// 3. At the "/name" path, display every entry in the animals collection, sorted by name
-app.get("/name", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything,
-  // but this time, sort it by name (1 means ascending order)
-  db.animals.find().sort({ name: 1 }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-    }
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  // Make a request for the news section of ycombinator
+  request("https://news.ycombinator.com/", function(error, response, html) {
+    // Load the html body from request into cheerio
+    var $ = cheerio.load(html);
+    // For each element with a "title" class
+    $(".title").each(function(i, element) {
+      // Save the text and href of each link enclosed in the current element
+      var title = $(element).children("a").text();
+      var link = $(element).children("a").attr("href");
+
+      // If this found element had both a title and a link
+      if (title && link) {
+        // Insert the data in the scrapedData db
+        db.scrapedData.insert({
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            // Log the error if one is encountered during the query
+            console.log(err);
+          }
+          else {
+            // Otherwise, log the inserted data
+            console.log(inserted);
+          }
+        });
+      }
+    });
   });
+
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
 });
 
-// 4. At the "/weight" path, display every entry in the animals collection, sorted by weight
-app.get("/weight", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything,
-  // but this time, sort it by weight (-1 means descending order)
-  db.animals.find().sort({ weight: -1 }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-    }
-  });
-});
 
-// Set the app to listen on port 3000
+// Listen on port 3000
 app.listen(3000, function() {
   console.log("App running on port 3000!");
 });
